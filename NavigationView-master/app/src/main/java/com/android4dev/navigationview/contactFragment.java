f6b0a1b7191d9df921b505ec720687d212b31eca
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2017-09-23 12:21:58 -0400

    added list view

--- NavigationView-master/app/src/main/java/com/android4dev/navigationview/contactFragment.java
+++ NavigationView-master/app/src/main/java/com/android4dev/navigationview/contactFragment.java
@@ -15,7 +15,7 @@ public class contactFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.addcontact,container,false);
+        View v = inflater.inflate(R.layout.addcontact, container, false);
         return v;
     }
 }
